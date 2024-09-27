bcd1878c715c1ac46ff60a002166e0d5684bf8bd
sainleon <josabele.17@gmail.com>
2024-09-26 22:35:13 -0500

    Se agregan clases modelo para mapeo de JSONJSONPlaceholder

---
+++ src/main/java/org/example/pruebatecnica/model/Geo.java
@@ -0,0 +1,13 @@
+package org.example.pruebatecnica.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Geo {
+    private String lat;
+    private String lng;
+}
--- src/main/java/org/example/pruebatecnica/model/User.java
+++ src/main/java/org/example/pruebatecnica/model/User.java
@@ -1,4 +1,19 @@
 package org.example.pruebatecnica.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    private int id;
+    private String name;
+    private String username;
+    private String email;
+    private Address address;
+    private String phone;
+    private String website;
+    private Company company;
 }
